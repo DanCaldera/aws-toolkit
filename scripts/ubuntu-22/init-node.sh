c261c8d509ad34f5ba4c5b8ab46cb0bba8d73f91
Daniel Caldera <bdcaldera@gmail.com>
2023-02-07 16:45:11 -0600

    Added install node example on ubuntu 22

--- scripts/ubuntu-22/init-node.sh
+++ scripts/ubuntu-22/init-node.sh
@@ -1,14 +1,19 @@
-# GIT
+#!/bin/bash
+
 sudo apt update
+
+# GIT
 sudo apt install git
 
 # nodejs
-sudo apt update
 sudo apt install nodejs
 sudo apt install npm
 
 # Install last stable version of nodejs
+# install this as root (sudo su)
+sudo su
 npm cache clean -f
 npm install -g n
 sudo n stable
 # sudo n latest
+exit
